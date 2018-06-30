366a0a0f9c5cee3dc498fa1d117b4087cc03f584
e <ernesto@MacBook-Air-de-ernesto.local>
2018-06-30 08:16:32 -0500

    aceptado

--- word_shit.c
+++ word_shit.c
@@ -18,21 +18,20 @@
 #define MAX_TAM_PAL 30
 #define MAX_TAM_LIN 80
 
-static inline char es_car_descon(char c) {
+static char es_car_descon(char c) {
 	return c == HYPHEN || c == APOSTROPHE;
 }
-static inline char es_car_valido(char c) {
+static char es_car_valido(char c) {
 	char r = 0;
 
 	assert(!isdigit(c));
 	if (isalpha(c) || es_car_descon(c)) {
 		r = 1;
 	}
-//	printf("car '%c' es %u\n", c, r);
 	return r;
 }
 
-static inline int tam_cad(char *s) {
+static int tam_cad(char *s) {
 	int r = 0;
 	while (s[r] != EOF && s[r] != '\n') {
 		r++;
@@ -40,7 +39,7 @@ static inline int tam_cad(char *s) {
 	return r;
 }
 
-static inline void proc_linea(char *linea, int *conteos, int *carrie) {
+static void proc_linea(char *linea, int *conteos, int *carrie) {
 	int carrie_int = *carrie;
 	int linea_tam = tam_cad(linea);
 	char uc = '\0';
@@ -82,7 +81,6 @@ static inline void proc_linea(char *linea, int *conteos, int *carrie) {
 		if (j < linea_tam) {
 			linea[j] = '\0';
 		}
-//		printf("caca '%s' tam %u car %u inv %u\n", linea + i, t, *carrie, inv_cnt);
 		i = j + 1;
 	}
 }
@@ -91,12 +89,12 @@ int main(void) {
 	char *linea = (char[MAX_TAM_LIN + 1] ) { '\0' };
 	int conteos[MAX_TAM_PAL + 1] = { 0 };
 	int carry = 0;
+	int i=0;
 
 	while (fgets(linea, MAX_TAM_LIN + 1, stdin)) {
-//		printf("a mi m gusta '%s':%u\n", linea, tam_cad(linea));
 		if (*linea == HASHIT) {
 			assert(!carry);
-			for (int i = 0; i < MAX_TAM_PAL + 1; i++) {
+			for (i = 0; i < MAX_TAM_PAL + 1; i++) {
 				assert(i || !conteos[i]);
 				if (conteos[i]) {
 					printf("%u %u\n", i, conteos[i]);
